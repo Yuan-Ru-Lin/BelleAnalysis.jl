c91c49910c976614eaf334d64ad25b74e318aa54
Yuan-Ru Lin <yuanruleonlin@gmail.com>
2022-09-11 16:54:17 +0800

    Change test file to B -> K pi

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using UnROOT
 using BelleAnalysis.Converter
 
-f = ROOTFile("samples/mdst_000001_prod00016666_task10020000001.root.udst.root")
+f = ROOTFile("samples/B2Kpi_10000.root")
 data, offsets = UnROOT.array(f, "tree/TrackFitResults/TrackFitResults.m_tau[5]", raw=true)
 events = UnROOT.splitup(data, offsets, TrackFitResult)
