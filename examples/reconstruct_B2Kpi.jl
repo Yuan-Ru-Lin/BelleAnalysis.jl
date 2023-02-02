57f8b1a984177dfa76e83d76d1cf6b0c470dc78c
Yuan-Ru Lin <yuanruleonlin@gmail.com>
2023-02-02 08:43:22 +0800

    Add smaller data file to git

--- examples/reconstruct_B2Kpi.jl
+++ examples/reconstruct_B2Kpi.jl
@@ -1,7 +1,7 @@
 using UnROOT
 using BelleAnalysis.Converter
 
-f = ROOTFile("samples/B2Kpi_10000.root")
+f = ROOTFile("samples/B2Kpi_100.root")
 data, offsets = UnROOT.array(f, "tree/TrackFitResults/TrackFitResults.m_tau[5]", raw=true)
 events = UnROOT.splitup(data, offsets, TrackFitResult)
 
